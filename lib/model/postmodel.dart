8892324e2958f982292ba6b9abc8ca9652dd8c2e
AnaghaMS1 <anaghasalbu05@gmail.com>
2024-02-12 12:03:07 +0530

    update

--- lib/model/postmodel.dart
+++ lib/model/postmodel.dart
@@ -9,7 +9,6 @@ List<Posts> postsFromJson(String str) => List<Posts>.from(json.decode(str).map((
 String postsToJson(List<Posts> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Posts {
-  String id;
   String name;
   String age;
   String phoneno;
@@ -17,10 +16,8 @@ class Posts {
   String pincode;
   String emailid;
   String password;
-  int v;
 
   Posts({
-    required this.id,
     required this.name,
     required this.age,
     required this.phoneno,
@@ -28,11 +25,9 @@ class Posts {
     required this.pincode,
     required this.emailid,
     required this.password,
-    required this.v,
   });
 
   factory Posts.fromJson(Map<String, dynamic> json) => Posts(
-    id: json["_id"],
     name: json["name"],
     age: json["age"],
     phoneno: json["phoneno"],
@@ -40,11 +35,9 @@ class Posts {
     pincode: json["pincode"],
     emailid: json["emailid"],
     password: json["password"],
-    v: json["__v"],
   );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
     "name": name,
     "age": age,
     "phoneno": phoneno,
@@ -52,6 +45,5 @@ class Posts {
     "pincode": pincode,
     "emailid": emailid,
     "password": password,
-    "__v": v,
   };
 }
