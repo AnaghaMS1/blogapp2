79c7bb70f1d6b6e8a7a45ebe224b22fdee06a33e
AnaghaMS1 <anaghasalbu05@gmail.com>
2024-02-12 11:25:28 +0530

    update

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -12,42 +12,64 @@ class signup extends StatelessWidget {
       body: Container(
         child: Column(
           children: [
-            Text("Emailid"),
+            Text("Name"),
             TextField(
               decoration: InputDecoration(
-                  hintText: "emailid"
-              ),
-
-            ),
-            SizedBox(height: 10,),   Text("Emailid"),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "emailid"
-              ),
-
-            ),
-            SizedBox(height: 10,),   Text("Emailid"),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "emailid"
-              ),
-
-            ),
-            SizedBox(height: 10,),   Text("Emailid"),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "emailid"
-              ),
-
-            ),
-            SizedBox(height: 10,),   Text("Emailid"),
-            TextField(
-              decoration: InputDecoration(
-                  hintText: "emailid"
+                  hintText: "name"
               ),
 
             ),
             SizedBox(height: 10,),
+            Text("Age"),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "age"
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            Text("Address"),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "address"
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            Text("phoneno"),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "phoneno"
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            Text("pincode"),
+            TextField(
+              decoration: InputDecoration(
+                  hintText: "pincode"
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            Text("Emailid"),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "eamild"
+              ),
+
+            ),
+            SizedBox(height: 10,),
+            Text("Password"),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "password"
+              ),
+            ),
+            ElevatedButton(onPressed: (){
+
+            }, child: Text("Submit")),
+            ElevatedButton(onPressed: (){}, child: Text("Back to login")),
 
           ],
         ),
